bed5878b0af038a12d9efb3576e56d58d2cb51dd
Shalom Metiboba <Shalommetiboba@gmail.com>
2024-12-15 21:57:42 +0100

    initial commit

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",            // If you have an index.html file in your project root
+    "./src/**/*.{js,jsx,ts,tsx}"  // This will cover all JavaScript and TypeScript files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
