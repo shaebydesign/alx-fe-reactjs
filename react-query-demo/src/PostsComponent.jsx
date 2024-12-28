9310f345e4d1e1819755323ce01b588e428b84a7
Shalom Metiboba <Shalommetiboba@gmail.com>
2024-12-28 19:07:51 +0100

    initial

---
+++ react-query-demo/src/PostsComponent.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return res.json();
+};
+
+function PostsComponent() {
+  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
