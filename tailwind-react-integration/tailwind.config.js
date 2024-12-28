b1e007d1903e377582be87346a5bd6721a2658d1
Shalom Metiboba <Shalommetiboba@gmail.com>
2024-12-28 18:07:44 +0100

    update

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure this points to the right files in your src folder
+  ],
+  darkMode: 'class', // You can set it to 'media' if you want automatic dark mode based on system preference
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
-
+};
