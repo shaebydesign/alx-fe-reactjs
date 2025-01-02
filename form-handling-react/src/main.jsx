a2d7f7947964d80cede97bf55b1de5b2690fee03
Shalom Metiboba <Shalommetiboba@gmail.com>
2025-01-02 15:22:51 +0100

    updated main

--- form-handling-react/src/main.jsx
+++ form-handling-react/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js'; // Corrected import to .js extension
+import App from './App.jsx'; // Corrected import to .jsx extension
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
