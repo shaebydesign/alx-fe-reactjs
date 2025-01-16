078ba26cafd4e0f0564b7ae3b93a22dc5c050619
shaebydesign <115163462+shaebydesign@users.noreply.github.com>
2025-01-16 13:36:36 +0100

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,11 @@
+// src/components/ProtectedRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';  // Import useAuth hook
 
-const ProtectedRoute = ({ element, isAuthenticated }) => {
-  // Check if the user is authenticated, render the component or redirect to login page
+const ProtectedRoute = ({ element }) => {
+  const isAuthenticated = useAuth();  // Use the custom hook to get authentication status
+
   return isAuthenticated ? element : <Navigate to="/login" />;
 };
 
